c6e16c7c2c1df4b2189dce6c6c85751769cc2bf8
Alessandro Muraro <akmuraro@gmail.com>
2019-02-04 14:46:34 +0100

    moving fetch to posts list

--- src/index.js
+++ src/index.js
@@ -6,25 +6,7 @@ import * as serviceWorker from './serviceWorker'
 
 const storedJson = sessionStorage.getItem('postsList')
 
-if (storedJson) {
-  console.log('session')
-  ReactDOM.render(
-    <App posts={JSON.parse(storedJson)} />,
-    document.getElementById('root')
-  )
-} else {
-  fetch(
-    'https://public-api.wordpress.com/rest/v1.1/sites/akmur.wordpress.com/posts/?number=100'
-  )
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(posts) {
-      sessionStorage.setItem('postsList', JSON.stringify(posts))
-      console.log('fetch')
-      ReactDOM.render(<App posts={posts} />, document.getElementById('root'))
-    })
-}
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -5,11 +5,52 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
 
 class Posts extends Component {
+  state = {
+    posts: {
+      posts: [],
+      loaded: false
+    }
+  }
+
+  componentDidMount() {
+    const storedJson = sessionStorage.getItem('postsList')
+
+    if (storedJson) {
+      this.setState({
+        posts: JSON.parse(storedJson),
+        loaded: true
+      })
+    } else {
+      fetch(
+        'https://public-api.wordpress.com/rest/v1.1/sites/akmur.wordpress.com/posts/?number=100'
+      )
+        .then(response => {
+          return response.json()
+        })
+        .then(posts => {
+          sessionStorage.setItem('postsList', JSON.stringify(posts))
+          this.setState({
+            posts,
+            loaded: true
+          })
+        })
+    }
+  }
   render() {
     return (
       <div className="postsList">
         <ul className="postsList__list">
-          {this.props.posts.posts.map((post, index) => (
+          {this.state.loaded ? (
+            ''
+          ) : (
+            <div className="loading">
+              <span role="img" aria-label="running man">
+                🏃‍♂️
+              </span>{' '}
+              Loading...
+            </div>
+          )}
+          {this.state.posts.posts.map((post, index) => (
             <li className="postsList__item" key={index}>
               <Link
                 className="postsList__link"
